f868bfb995eaec3d429f6b9e01dfe933ca05de90
kevjose <kevin.jose@letsventure.com>
2017-01-03 16:34:32 +0530

    added build.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const Server = require('./server.js')
 const port = (process.env.OPENSHIFT_NODEJS_PORT || 8080)
 const app = Server.app()
 
-if (process.env.NODE_ENV !== 'production') {
+if (app.get('env') === 'production') {
   const webpack = require('webpack')
   const webpackDevMiddleware = require('webpack-dev-middleware')
   const webpackHotMiddleware = require('webpack-hot-middleware')
